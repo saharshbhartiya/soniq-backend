6f0ef9216b19707e5eb355ba5ebdfbceafc680c4
Saharsh Bhartiya <saharshbhartiya@gmail.com>
2025-12-07 19:27:23 +0530

    Initial backend setup

---
+++ src/main/java/com/saharsh/soniq/SoniqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saharsh.soniq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoniqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoniqBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saharsh/soniq/SoniqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saharsh.soniq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoniqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
